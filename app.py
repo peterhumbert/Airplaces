cc7eb7e192aee519356a8230741fdf38c6f1c8dc
Peter Humbert <humbe037@umn.edu>
2017-09-21 12:36:07 -0500

    cleanup after docker test

--- app.py
+++ app.py
@@ -13,31 +13,27 @@ apiKey = config.get('credentials','fa_api_key')
 username = config.get('credentials','fa_username')
 fxmlUrl = "https://flightxml.flightaware.com/json/FlightXML3/"
 
-# define app endpoint
-@app.route("/")
-def test():
-	return username
-
+# define app endpoint for testing
 @app.route("/<airline>/<flightno>")
 def flightDataTest(airline, flightno):
 	return airline + flightno
 
+# define main app endpoint
 @app.route("/<airline>/<flightno>/<date>")
-def hello_user(airline, flightno, date):
-	try:
-		payload = {'ident':airline+flightno, 'howMany':'30'}
-		response = requests.get(fxmlUrl + "FlightInfoStatus", params=payload, auth=(username, apiKey))
-	except Exception as inst:
-		return inst
+def locationByFlightNo(airline, flightno, date):
+	# lookup the requested flight
+	payload = {'ident':airline + flightno, 'howMany':'30'}
+	response = requests.get(fxmlUrl + "FlightInfoStatus", params=payload, auth=(username, apiKey))
 
+	output = ""
 	if response.status_code == 200:
 		decodedResponse = response.json()
-		return decodedResponse
-		# for flight in decodedResponse['FlightInfoStatusResult']['flights']:
-		# 	print("{} ({})\t{} {} ({})\t{}\t{}\n{}\n".format(flight['ident'], flight['aircrafttype'],
-	    #                                                        flight['origin']['airport_name'], flight['origin']['code'],
-	    #                                                        flight['destination']['code'], flight['actual_departure_time'],
-	    #                                                        flight['status'],flight['faFlightID']))
+		for flight in decodedResponse['FlightInfoStatusResult']['flights']:
+			output = output + "{} ({})\t{} {} ({})\t{}\t{}\n{}\n".format(flight['ident'], flight['aircrafttype'],
+	                                                           flight['origin']['airport_name'], flight['origin']['code'],
+	                                                           flight['destination']['code'], flight['actual_departure_time'],
+	                                                           flight['status'],flight['faFlightID'])
+		return output
 	else:
 		return "There was an error retrieving the data from the server."
 
@@ -46,5 +42,6 @@ def hello_user(airline, flightno, date):
 def locationByRoute(origin, destination, date):
 	return 'NOT YET IMPLEMENTED'
 
+# run the app
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
