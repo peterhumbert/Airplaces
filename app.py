cf994dabcc5b6baef05b2df45d2bbe53ef0af69d
Peter Humbert <humbe037@umn.edu>
2017-09-22 13:04:41 -0500

    prelim API date conversion and app skeleton

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 import requests
 import configparser
 import json
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -19,8 +20,11 @@ def flightDataTest(airline, flightno):
 	return airline + flightno
 
 # define main app endpoint
-@app.route("/<airline>/<flightno>/<date>")
-def locationByFlightNo(airline, flightno, date):
+@app.route("/<airline>/<flightno>/<timestamp>")
+def locationByFlightNo(airline, flightno, timestamp):
+	# convert inputted date to datetime
+	photoDate = convertAPITimestamp(timestamp)
+
 	# lookup the requested flight
 	payload = {'ident':airline + flightno, 'howMany':'30'}
 	response = requests.get(fxmlUrl + "FlightInfoStatus", params=payload, auth=(username, apiKey))
@@ -29,6 +33,10 @@ def locationByFlightNo(airline, flightno, date):
 	if response.status_code == 200:
 		decodedResponse = response.json()
 		for flight in decodedResponse['FlightInfoStatusResult']['flights']:
+			if flight['status'] == 'En':
+				# considering an en-route flight
+			elif flight['status'] == 'Arrived':
+				# consider an arrived flight
 			output = output + "{} ({})\t{} {} ({})\t{}\t{}\n{}\n".format(flight['ident'], flight['aircrafttype'],
 	                                                           flight['origin']['airport_name'], flight['origin']['code'],
 	                                                           flight['destination']['code'], flight['actual_departure_time'],
@@ -40,7 +48,27 @@ def locationByFlightNo(airline, flightno, date):
 # define future app endpoint
 @app.route("/<origin>/<destination>/<date>")
 def locationByRoute(origin, destination, date):
+	# TODO consider using FindFlight API endpoint
 	return 'NOT YET IMPLEMENTED'
+
+# ==================================================
+# HELPER FUNCTIONS
+# ==================================================
+
+# for a specified datetime, interpolate the positions (lat,long) associated with
+# the two immediately adjacent datetimes
+def interpolatePosition(specifieddt, dt1, dt2, pos1, pos2):
+	# TODO
+	return 'NOT YET IMPLEMENTED'
+
+# convert a string of formay YYYYMMDDhhmmss to a datetime object
+def convertAPITimestamp(timestamp):
+	return datetime.datetime(int(date[0:4]),int(date[4:6]),int(date[6:8]),
+		int(date[8:10]),int(date[10:12]),int(date[12:14]))
+
+# convert a FlightAware timestamp to a datetime object
+def convertFATimestamp(timestamp):
+	# TODO
 
 # run the app
 if __name__ == "__main__":
