f57d916aea6cac4b53feb77ad8dd1f3adb294de4
Peter Humbert <humbe037@umn.edu>
2017-09-24 00:37:42 -0500

    timezone handling and track search implemented

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 import requests
 import configparser
 import json
-from datetime import datetime
+from datetime import datetime, tzinfo
 
 app = Flask(__name__)
 
@@ -13,6 +13,20 @@ config.read('airplaces.cfg')
 apiKey = config.get('credentials','fa_api_key')
 username = config.get('credentials','fa_username')
 fxmlUrl = "https://flightxml.flightaware.com/json/FlightXML3/"
+
+utc = UTC()
+
+class UTC(tzinfo):
+    """UTC"""
+
+    def utcoffset(self, dt):
+        return timedelta(0)
+
+    def tzname(self, dt):
+        return "UTC"
+
+    def dst(self, dt):
+        return timedelta(0)
 
 # define app endpoint for testing
 @app.route("/<airline>/<flightno>")
@@ -44,45 +58,90 @@ def locationByFlightNo(airline, flightno, timestamp):
 				departTime = convertFATimestamp(flight['actual_departure_time'])
 
 				if (photoTime > departTime):
+					# flight found!
 					matchFAID = flight['faFlightID']
+					photoTime -= getUTCoffset(flight['actual_departure_time'])
 			elif flight['status'] == 'Arrived':
 				# consider an arrived flight
 				arriveTime = convertFATimestamp(flight['actual_arrival_time'])
 				arriveTime += tzConversionFactor(
 					flight['actual_departure_time'],
-					flight['actual_arrival_time'])
+					flight['actual_arrival_time'])  # convert arriveTime to
+													# departure timezone
 				departTime = convertFATimestamp(flight['actual_departure_time'])
 
-				# 	check if photo was taken while i-th flight was airborne
+				# check if photo was taken while i-th flight was airborne
 				# assumption: flight numbers associated with two routes won't
 				# cause overlap between the ranges of airborne times for their
 				# respective timezones because these flight numbers are
 				# expected to only cover A-to-B and B-to-C routes. They
 				# therefore use different timezones.
 				if (photoTime > departTime) and (photoTime < arriveTime):
+					# flight found!
 					matchFAID = flight['faFlightID']
+					photoTime -= getUTCoffset(flight['actual_departure_time'])
 
 			# output = output + "{} ({})\t{} {} ({})\t{}\t{}\n{}\n".format(flight['ident'], flight['aircrafttype'],
 	        #                                                    flight['origin']['airport_name'], flight['origin']['code'],
 	        #                                                    flight['destination']['code'], flight['actual_departure_time'],
 	        #                                                    flight['status'],flight['faFlightID'])
 			i += 1
+			## end of while loop
 
-		if (not matchFAID == None):
+		if (matchFAID != None):
 			# the flight was found
+			payload = {'ident':matchFAID}
+			response = requests.get(fxmlUrl + "GetFlightTrack", params=payload,
+				auth=(username, apiKey))
 
+			if response.status_code == 200:
+				decodedResponse = response.json()
+
+				j = 0
+				tBefore = None	# for timestamp immediately Before photoTime
+				tAfter = None	# for timestamp immediately After photoTime
+				posBefore = None
+				posAfter = None
+				while (j < len(decodedResponse['GetFlightTrackResult']['tracks'])) - 1
+					and (tBefore != None) and (tAfter != None):
+					track1 = decodedResponse['GetFlightTrackResult']['tracks'][j]
+					track2 = decodedResponse['GetFlightTrackResult']['tracks'][j+1]
+					if (datetime.fromtimestamp(track1['timestamp'],utc) < photoTime) and
+						(datetime.fromtimestamp(track2['timestamp'],utc) > photoTime):
+						# photo was taken between the two current track entries
+						tBefore = datetime.fromtimestamp(track1['timestamp'],utc)
+						tAfter = datetime.fromtimestamp(track2['timestamp'],utc)
+						posBefore = {'longitude': track1['longitude'],
+							'latitude': track1['latitude']}
+						posAfter = {'longitude': track2['longitude'],
+							'latitude': track2['latitude']}
+
+						photoPos = interpolatePosition(photoTime, tBefore,
+							tAfter, posBefore, posAfter)
+
+						return json.dumps({'APAPI_status':0,
+							'latitude':photoPos['latitude'],
+							'longitude':photoPos['longitude']})
+					j += 1
+					## end of the while loop
+				# outside the while loop = position not determined
+				return json.dumps({'APAPI_status':-4})
+			else:
+				# GET failed
+				return json.dumps({'APAPI_status':-3})
 		else:
 			# no matching flight was found
 			return json.dumps({'APAPI_status':-2})
 		return output
 	else:
+		# GET failed
 		return json.dumps({'APAPI_status':-1})
 
 # define future app endpoint
-@app.route("/<origin>/<destination>/<date>")
-def locationByRoute(origin, destination, date):
-	# TODO consider using FindFlight API endpoint
-	return 'NOT YET IMPLEMENTED'
+# @app.route("/<origin>/<destination>/<date>")
+# def locationByRoute(origin, destination, date):
+# 	# TODO consider using FindFlight API endpoint
+# 	return 'NOT YET IMPLEMENTED'
 
 # ==================================================
 # HELPER FUNCTIONS
@@ -120,6 +179,11 @@ def tzConversionFactor(departTimestamp, arriveTimestamp):
 	arriveUTCoffset = (arriveTimestamp['localtime']
 		- arriveTimestamp['epoch'])/3600
 	return timedelta(hours=departUTCoffset-arriveUTCoffset)
+
+# return the UTC offset of a {localtime, epoch} timestamp;
+# to be used with departure time to convert photo time to UTC
+def getUTCoffset(timestamp):
+	return timedelta(hours=(timestamp['localtime']-timestamp['epoch'])/3600)
 
 # run the app
 if __name__ == "__main__":
