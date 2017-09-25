6adcdfef35cdfce1b9855b1a4610f6a18493a380
Peter Humbert <humbe037@umn.edu>
2017-09-24 19:08:43 -0500

    implement try/except blocks and bug fixes

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import requests
 import configparser
 import json
 from datetime import datetime, tzinfo, timedelta
+#import pytz
+
+#utc=pytz.UTC
 
 app = Flask(__name__)
 
@@ -49,48 +52,43 @@ def locationByFlightNo(airline, flightno, timestamp):
 		decodedResponse = response.json()
 		i = 0
 		matchFAID = None
-		while (i < len(decodedResponse['FlightInfoStatusResult']['flights'])
-			and matchFAID == None):
-			flight = decodedResponse['FlightInfoStatusResult']['flights'][i]
+		try:
+			while (i < len(decodedResponse['FlightInfoStatusResult']['flights'])
+				and matchFAID == None):
+				flight = decodedResponse['FlightInfoStatusResult']['flights'][i]
 
-			if flight['status'] == 'En':
-				# considering an en-route flight
-				departTime = convertFATimestamp(flight['actual_departure_time'])
+				if flight['status'] == 'En':
+					# considering an en-route flight
+					departTime = convertFATimestamp(flight['actual_departure_time'])
 
-				if (photoTime > departTime):
-					# flight found!
-					matchFAID = flight['faFlightID']
-					photoTime -= getUTCoffset(flight['actual_departure_time'])
-			elif flight['status'] == 'Arrived':
-				# consider an arrived flight
-				arriveTime = convertFATimestamp(flight['actual_arrival_time'])
-				arriveTime += tzConversionFactor(
-					flight['actual_departure_time'],
-					flight['actual_arrival_time'])  # convert arriveTime to
-													# departure timezone
-				departTime = convertFATimestamp(flight['actual_departure_time'])
-				print(departTime)
-				print(arriveTime)
-				print(photoTime)
-				print('')
+					if (photoTime > departTime):
+						# flight found!
+						matchFAID = flight['faFlightID']
+						photoTime -= getUTCoffset(flight['actual_departure_time'])
+				elif flight['status'] == 'Arrived':
+					# consider an arrived flight
+					arriveTime = convertFATimestamp(flight['actual_arrival_time'])
+					arriveTime += tzConversionFactor(
+						flight['actual_departure_time'],
+						flight['actual_arrival_time'])  # convert arriveTime to
+														# departure timezone
+					departTime = convertFATimestamp(flight['actual_departure_time'])
 
-				# check if photo was taken while i-th flight was airborne
-				# assumption: flight numbers associated with two routes won't
-				# cause overlap between the ranges of airborne times for their
-				# respective timezones because these flight numbers are
-				# expected to only cover A-to-B and B-to-C routes. They
-				# therefore use different timezones.
-				if (photoTime > departTime) and (photoTime < arriveTime):
-					# flight found!
-					matchFAID = flight['faFlightID']
-					photoTime -= getUTCoffset(flight['actual_departure_time'])
-
-			# output = output + "{} ({})\t{} {} ({})\t{}\t{}\n{}\n".format(flight['ident'], flight['aircrafttype'],
-	        #                                                    flight['origin']['airport_name'], flight['origin']['code'],
-	        #                                                    flight['destination']['code'], flight['actual_departure_time'],
-	        #                                                    flight['status'],flight['faFlightID'])
-			i += 1
-			## end of while loop
+					# check if photo was taken while i-th flight was airborne
+					# assumption: flight numbers associated with two routes won't
+					# cause overlap between the ranges of airborne times for their
+					# respective timezones because these flight numbers are
+					# expected to only cover A-to-B and B-to-C routes. They
+					# therefore use different timezones.
+					if (photoTime > departTime) and (photoTime < arriveTime):
+						# flight found!
+						matchFAID = flight['faFlightID']
+						photoTime -= getUTCoffset(flight['actual_departure_time'])
+				i += 1
+				## end of while loop
+		except Exception as e:
+			# FlightInfoStatus call succeeded but didn't find any matching flights
+			return json.dumps({'APAPI_status':-5})
 
 		if (matchFAID != None):
 			# the flight was found
@@ -101,35 +99,41 @@ def locationByFlightNo(airline, flightno, timestamp):
 			if response.status_code == 200:
 				decodedResponse = response.json()
 
-				j = 0
-				tBefore = None	# for timestamp immediately Before photoTime
-				tAfter = None	# for timestamp immediately After photoTime
-				posBefore = None
-				posAfter = None
-				while (j < len(decodedResponse['GetFlightTrackResult']['tracks']) - 1
-					and (tBefore != None) and (tAfter != None)):
-					track1 = decodedResponse['GetFlightTrackResult']['tracks'][j]
-					track2 = decodedResponse['GetFlightTrackResult']['tracks'][j+1]
-					if (datetime.fromtimestamp(track1['timestamp'],utc) < photoTime and
-						datetime.fromtimestamp(track2['timestamp'],utc) > photoTime):
-						# photo was taken between the two current track entries
-						tBefore = datetime.fromtimestamp(track1['timestamp'],utc)
-						tAfter = datetime.fromtimestamp(track2['timestamp'],utc)
-						posBefore = {'longitude': track1['longitude'],
-							'latitude': track1['latitude']}
-						posAfter = {'longitude': track2['longitude'],
-							'latitude': track2['latitude']}
+				try:
+					j = 0
+					tBefore = None	# for timestamp immediately Before photoTime
+					tAfter = None	# for timestamp immediately After photoTime
+					posBefore = None
+					posAfter = None
+					while (j < (len(decodedResponse['GetFlightTrackResult']['tracks']) - 1)
+						and (tBefore == None) and (tAfter == None)):
+						track1 = decodedResponse['GetFlightTrackResult']['tracks'][j]
+						track2 = decodedResponse['GetFlightTrackResult']['tracks'][j+1]
 
-						photoPos = interpolatePosition(photoTime, tBefore,
-							tAfter, posBefore, posAfter)
+						if (datetime.fromtimestamp(track1['timestamp'],utc) < photoTime and
+							datetime.fromtimestamp(track2['timestamp'],utc) > photoTime):
+							# photo was taken between the two current track entries
+							tBefore = datetime.fromtimestamp(track1['timestamp'],utc)
+							tAfter = datetime.fromtimestamp(track2['timestamp'],utc)
 
-						return json.dumps({'APAPI_status':0,
-							'latitude':photoPos['latitude'],
-							'longitude':photoPos['longitude']})
-					j += 1
-					## end of the while loop
-				# outside the while loop = position not determined
-				return json.dumps({'APAPI_status':-4})
+							posBefore = {'longitude': track1['longitude'],
+								'latitude': track1['latitude']}
+							posAfter = {'longitude': track2['longitude'],
+								'latitude': track2['latitude']}
+
+							photoPos = interpolatePosition(photoTime, tBefore,
+								tAfter, posBefore, posAfter)
+
+							return json.dumps({'APAPI_status':0,
+								'latitude':photoPos['latitude'],
+								'longitude':photoPos['longitude']})
+						j += 1
+						## end of the while loop
+					# outside the while loop = position not determined
+					return json.dumps({'APAPI_status':-4})
+				except Exception as e:
+					# GetFlightTrack call succeeded but didn't find track data
+					return json.dumps({'APAPI_status':-6})
 			else:
 				# GET failed
 				return json.dumps({'APAPI_status':-3})
@@ -155,14 +159,16 @@ def locationByFlightNo(airline, flightno, timestamp):
 # the two immediately adjacent datetimes
 def interpolatePosition(specifieddt, dt1, dt2, pos1, pos2):
 	multiplier = (specifieddt-dt1)/(dt2-dt1)
-	return {'latitude':pos1['latitude'] + multiplier*pos2['latitude'],
-		'longitude':pos1['longitude'] + multiplier*pos2['longitude']}
+	latdiff = pos2['latitude']-pos1['latitude']
+	londiff = pos2['longitude']-pos1['longitude']
+	return {'latitude':pos1['latitude'] + multiplier*latdiff,
+		'longitude':pos1['longitude'] + multiplier*londiff}
 
 # convert a string of formay YYYYMMDDhhmmss to a datetime object
 def convertAPITimestamp(timestamp):
 	return datetime(int(timestamp[0:4]), int(timestamp[4:6]),
 		int(timestamp[6:8]), int(timestamp[8:10]), int(timestamp[10:12]),
-		int(timestamp[12:14]))
+		int(timestamp[12:14]), 0, utc)
 
 # convert a FlightAware timestamp to a datetime object
 def convertFATimestamp(timestamp):
@@ -175,7 +181,7 @@ def convertFATimestamp(timestamp):
 		hour += 12
 
 	return datetime(int(date[6:10]), int(date[0:2]), int(date[3:5]),
-		hour, minute)
+		hour, minute, 0, 0, utc)
 
 # convert arriveTime to departTime's timezone
 def tzConversionFactor(departTimestamp, arriveTimestamp):
